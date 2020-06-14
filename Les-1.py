1f1f70e0a532fd333517b285bab28abd291761c4
Stas <scron244@bk.ru>
2020-06-15 00:05:16 +0300

    less-1

---
+++ Les-1.py
@@ -0,0 +1,8 @@
+catalog_id_1 = 14
+catalog_id_2 = 15
+my_float = 3.14 # это комментарий ну предположим my_float = числу ПИ
+my_str_1 = "Stanislav"
+my_str_2 = "Poletavkin"
+
+print("Hello world")
+print(my_str_1, my_str_2)
