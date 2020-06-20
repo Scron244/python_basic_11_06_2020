606cdab275833d7f59ee1b4cc9c8918c3619d2ea
Stas <scron244@bk.ru>
2020-06-20 13:04:35 +0300

    less-1

--- homework/Les_1/HW-1.py
+++ homework/Les_1/HW-1.py
@@ -1,16 +1,8 @@
-catalog_id_1 = 14
-catalog_id_2 = 15
-my_float = 3.14 # это комментарий ну предположим my_float = числу ПИ
-my_str_1 = "Stanislav"
-my_str_2:str = "Poletavkin"
-
-#print("Hello world")
-#print(my_str_1, my_str_2)
-pass
-print("Hello")
-print("Fuck code")
+"Home work 1"
 
 #1. Поработайте с переменными, создайте несколько,
 #выведите на экран, запросите у пользователя несколько чисел и строк и сохраните в переменные,
 #выведите на экран.
 
+print("Hello")
+print("Fuck code")
