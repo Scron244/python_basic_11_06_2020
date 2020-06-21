cff4ec1d83ac614a2f49e4deecd4c4f19142c883
Stas <scron244@bk.ru>
2020-06-21 16:52:04 +0300

    lesson 2

--- HW/HW_2-4.py
+++ HW/HW_2-4.py
@@ -4,3 +4,15 @@
 # Вывести каждое слово с новой строки.
 # Строки необходимо пронумеровать.
 # Если в слово длинное, выводить только первые 10 букв в слове.
+
+user_str = input("введите строку ")
+user_word = []
+num = 1
+for el in range(user_str.count(' ') + 1):
+    user_word = user_str.split()
+    if len(str(user_word)) <= 10:
+        print(f" {num} {user_word [el]}")
+        num += 1
+    else:
+        print(f" {num} {user_word [el] [0:10]}")
+        num += 1
