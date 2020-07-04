5b902837676f8f1da068362680b501ea33bb246e
Stas <scron244@bk.ru>
2020-07-04 15:07:30 +0300

    lesson 4

--- HW4/HW 4-6.py
+++ HW4/HW 4-6.py
@@ -9,12 +9,19 @@
 # Во втором также необходимо предусмотреть условие, при котором повторение элементов списка будет прекращено.
 
 from itertools import count
-
-for el in count(int(input('Введите стартовое число '))):
-    print(el) # внимание - беконечный цикл!
-
 from itertools import cycle
 
-my_list = [True, 'ABC', 123, None]
-for el in cycle(my_list):
-    print(el)
+def my_count_func(start_number, stop_number):
+    for el in count(start_number):
+        if el > stop_number:
+            break
+        else:
+            print(el)
+def my_cycle_func(my_list, iteration):
+    i = 0
+    iter = cycle(my_list)
+    while i < iteration:
+        print(next(iter))
+        i+=1
+my_count_func(start_number = int(input("enter start number: ")), stop_number = int(input("enter stop number: ")))
+my_cycle_func(my_list = [1, 2], iteration = int(input("enter iteration: ")))
