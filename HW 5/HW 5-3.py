d71fba761db50db83baf49c04345c1d439f7630d
Stas <scron244@bk.ru>
2020-07-05 18:24:57 +0300

    lesson 5

--- HW 5/HW 5-3.py
+++ HW 5/HW 5-3.py
@@ -4,8 +4,18 @@
 # Определить, кто из сотрудников имеет оклад менее 20 тыс., вывести фамилии этих сотрудников.
 # Выполнить подсчет средней величины дохода сотрудников.
 
+with open('file5_3.txt', 'r') as my_file:
+    sal = []
+    poor = []
+    my_list = my_file.read().split('\n')
+    for i in my_list:
+        i = i.split()
+    if int(i[1]) < 20000:
+        poor.append(i[0])
+    sal.append(i[1])
+    print(f'Оклад меньше 20.000 {poor}, средний оклад {sum(map(int, sal)) / len(sal)}')
 
-my_file = open('file5_3.txt', 'r')
-content = my_file.readlines()
-print(content)
-my_file.close()
+#my_file = open('file5_3.txt', 'r') - просто вывод содержимого файла
+#content = my_file.readlines()
+#print(content)
+#my_file.close()
